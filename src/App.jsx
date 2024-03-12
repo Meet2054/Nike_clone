b15f6fc4be8443f98888b4369bb9ad21e53422e7
Meet2054 <meetaparekh2003@gmail.com>
2024-03-12 14:26:34 +0530

    Initial Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main>
+      Nav
+      <section className=" xl:padding-1 wide:padding-r padding-b ">
+        Hero
+      </section>
+      <section className=" padding">
+        Popular Product
+      </section>
+      <section className=" padding">
+        SuperQulity Product
+      </section>
+      <section className=" padding-x py-10">
+        Service
+      </section>
+      <section className=" padding">
+        Special Offer
+      </section>
+      <section className=" padding bg-pale-blue">
+        Coustemer Review
+      </section>
+      <section className=" padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   )
 }
